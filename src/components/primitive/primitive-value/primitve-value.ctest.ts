747bf996a60078435772fc5c9e473b08106ca102
zework <florian@hehlen.name>
2024-07-04 23:50:15 +0200

    adding tests

--- src/components/primitive/primitive-value/primitve-value.ctest.ts
+++ src/components/primitive/primitive-value/primitve-value.ctest.ts
@@ -36,8 +36,65 @@ describe('fhir primitive value', () => {
 
   })
 
+  it('should display link', async () => {
 
-  it('should display text with fixed width', async () => {
+    const el = await fixture<PrimitiveValue>(html`
+      <fhir-value text="hello" link="https://google.com"></fhir-value >
+    `)
+
+    await expect(el).shadowDom.to.equal(`
+            <div>
+             <a href="https://google.com">
+                <slot name="before"></slot>
+                hello
+                <slot name="after"></slot>
+             </a>
+            </div>
+      `)
+
+  })
+
+  it('should display context', async () => {
+
+    const el = await fixture<PrimitiveValue>(html`
+      <fhir-value text="" placeholder="missing value"></fhir-value >
+    `)
+
+    await expect(el).shadowDom.to.equal(`
+            <div>
+              <span class="placeholder">
+                missing value
+              </span>
+            </div>
+
+
+      `)
+
+  })
+
+  it('should display prefix and suffixes', async () => {
+
+    const el = await fixture<PrimitiveValue>(html`
+      <fhir-value text="100">
+        <span slot="before">over</span >
+        <span slot="after">percent</span >
+      </fhir-value >
+    `)
+
+    await expect(el).shadowDom.to.equal(`
+            <div>
+              <span class="placeholder">
+                missing value
+              </span>
+            </div>
+
+
+      `)
+
+  })
+
+
+  it('should display text with hide-overflow', async () => {
 
     const el = await fixture<PrimitiveValue>(html`
       <fhir-value text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Tellus in hac habitasse platea. Posuere urna nec tincidunt praesent semper feugiat nibh. Tortor pretium viverra suspendisse potenti nullam ac tortor. Fusce id velit ut tortor pretium viverra suspendisse potenti. Enim eu turpis egestas pretium aenean pharetra. Non consectetur a erat nam at lectus. Amet est placerat in egestas erat imperdiet sed." variant="hide-overflow"></fhir-value >
@@ -61,7 +118,13 @@ describe('fhir primitive value', () => {
       expect.fail('shadow root content not found')
     }
 
-    // const mouseoverEvent = new Event('mouseover')
+    //TODO get hover test to work
+
+    // const mouseoverEvent = new MouseEvent('mouseover', {
+    //   view: window,
+    //   bubbles: true,
+    //   cancelable: true
+    // })
     //
     // el.dispatchEvent(mouseoverEvent)
     //
@@ -75,6 +138,33 @@ describe('fhir primitive value', () => {
     // } else {
     //   expect.fail('shadow root content not found')
     // }
+
+
+  })
+
+  it('should display text with fixed width', async () => {
+
+    const el = await fixture<PrimitiveValue>(html`
+      <fhir-value text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Tellus in hac habitasse platea. Posuere urna nec tincidunt praesent semper feugiat nibh. Tortor pretium viverra suspendisse potenti nullam ac tortor. Fusce id velit ut tortor pretium viverra suspendisse potenti. Enim eu turpis egestas pretium aenean pharetra. Non consectetur a erat nam at lectus. Amet est placerat in egestas erat imperdiet sed." variant="fixed-width"></fhir-value >
+    `)
+
+    await expect(el).shadowDom.to.equal(`
+             <div class="fixed-width">
+                <slot name="before"></slot>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Tellus in hac habitasse platea. Posuere urna nec tincidunt praesent semper feugiat nibh. Tortor pretium viverra suspendisse potenti nullam ac tortor. Fusce id velit ut tortor pretium viverra suspendisse potenti. Enim eu turpis egestas pretium aenean pharetra. Non consectetur a erat nam at lectus. Amet est placerat in egestas erat imperdiet sed.
+                <slot name="after"></slot>
+             </div>
+      `)
+
+    let div: HTMLDivElement | null | undefined = el.shadowRoot?.querySelector('div.fixed-width')
+    if (div) {
+      const { w } = dimmensions(div, 'rem')
+
+      await expect(w).to.equal(40)
+    } else {
+      expect.fail('shadow root content not found')
+    }
+
 
 
   })
--- stories/system/primitive_elements/value.stories.ts
+++ stories/system/primitive_elements/value.stories.ts
@@ -15,16 +15,23 @@ export const ValueElement: Story = {
   render: () => html`
     <h2 >Value</h2 >
     <fhir-wrapper >
-      <h3 >typical</h3 >
+      <h3 >simple value</h3 >
       <fhir-value text="John Smith"></fhir-value >
-      <h3 >link</h3 >
+      <h3 >link value</h3 >
       <fhir-value text="John Smith" link="http://foo.com/patient/1234"></fhir-value >
-      <h3 >long value</h3 >
+      <h3 >placeholder value</h3 >
+      <fhir-value text="" placeholder="missing value"></fhir-value >
+      <h3 >prefix and suffix value</h3 >
+      <fhir-value text="100">
+        <span slot="before">></span >
+        <span slot="after">%</span >
+      </fhir-value >
+      <h3 >long value value</h3 >
       <fhir-value text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida."></fhir-value >
       <h3 >fixed-width variant on long value</h3 >
       <fhir-value text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida." variant="fixed-width"></fhir-value >
       <h3 >hide overflow variant on long value</h3 >
-      <fhir-value text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida." variant="hide-overflow"></fhir-value >
+      <fhir-value id="debug-target" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Amet luctus venenatis lectus magna fringilla urna porttitor. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Tellus in hac habitasse platea. Posuere urna nec tincidunt praesent semper feugiat nibh. Tortor pretium viverra suspendisse potenti nullam ac tortor. Fusce id velit ut tortor pretium viverra suspendisse potenti. Enim eu turpis egestas pretium aenean pharetra. Non consectetur a erat nam at lectus. Amet est placerat in egestas erat imperdiet sed." variant="hide-overflow"></fhir-value >
     </fhir-wrapper >
   `
 }
