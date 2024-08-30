e48969262cec3e0bc5f8b748910f5650db3c3e3a
zework <florian@hehlen.name>
2024-08-31 01:02:12 +0200

    #50: added narrative rendering for display mode
    * added rendering
    * added tests

--- src/internal/index.ts
+++ src/internal/index.ts
@@ -1,7 +1,7 @@
 // created by autobarrel, do not modify directly
 
 export * from './BaseElement'
-export * from './base/'
-export * from './contexts/'
+export * from './base'
+export * from './contexts'
 export * from './customization'
 export * from './resource'
--- src/internal/resource/index.ts
+++ src/internal/resource/index.ts
@@ -1,8 +1,8 @@
 // created by autobarrel, do not modify directly
 
-export * from './backbone.data'
 export * from './Backbone'
-export * from './domain-resource.data'
 export * from './DomainResource'
-export * from './renderResourceComponent'
 export * from './Resource'
+export * from './backbone.data'
+export * from './domain-resource.data'
+export * from './renderResourceComponent'
