6dc35c5833f3ac7636bf911203966b104c90048b
zework <florian@hehlen.name>
2025-02-09 12:42:28 +0100

    #91 Primitive date, time, and datetime formatting
    - added formatter for fhir date

---
+++ packages/library/src/components/primitive/type-presenters/asFormattedDate.test.ts
@@ -0,0 +1,40 @@
+import {describe, expect, it} from 'vitest'
+import {asFormattedDate}      from './asFormattedDate'
+
+
+
+describe('asFormattedDate', () => {
+  it('returns formatted string for YYYY-MM-DD ', () => {
+    let formatted: string = asFormattedDate({ date: '1905-08-23' })
+    expect(formatted).toBe('23.08.1905')
+
+    formatted = asFormattedDate({ date: '1905-08-23', order: 'MDY', separator: '/' })
+    expect(formatted).toBe('08/23/1905')
+
+    formatted = asFormattedDate({ date: '1905-08-23', order: 'DMY', separator: '-', monthFormat: 'medium' })
+    expect(formatted).toBe('23-Aug-1905')
+  })
+
+  it('returns formatted string for YYYY-MM ', () => {
+    let formatted: string = asFormattedDate({ date: '1905-08' })
+    expect(formatted).toBe('08.1905')
+
+    formatted = asFormattedDate({ date: '1905-08', order: 'MDY', separator: '/' })
+    expect(formatted).toBe('08/1905')
+
+    formatted = asFormattedDate({ date: '1905-08', order: 'DMY', separator: '-', monthFormat: 'medium' })
+    expect(formatted).toBe('Aug-1905')
+  })
+
+  it('returns formatted string for YYYY ', () => {
+    let formatted: string = asFormattedDate({ date: '1905' })
+    expect(formatted).toBe('1905')
+
+    formatted = asFormattedDate({ date: '1905', order: 'MDY', separator: '/' })
+    expect(formatted).toBe('1905')
+
+    formatted = asFormattedDate({ date: '1905', order: 'DMY', separator: '-', monthFormat: 'medium' })
+    expect(formatted).toBe('1905')
+  })
+
+})
