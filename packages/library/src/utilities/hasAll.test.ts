25f044f2dce1e8e062a4efa62b65de544b3e9ccd
Florian Hehlen <florian@hehlen.name>
2025-06-29 13:16:16 +0200

    fix: treat falsy values as defined in hasAll (#126)
    * fix: treat falsy values as defined in hasAll
    * chore: add changeset for hasAll patch

--- packages/library/src/utilities/hasAll.test.ts
+++ packages/library/src/utilities/hasAll.test.ts
@@ -4,6 +4,7 @@ import {hasAll}                 from './hasAll'
 describe('function: has all', () => {
 
   const obj = { a: 'aa', b: 'bb', c: 'cc', d: 'dd' }
+  const falsyObj = { a: 0, b: '', c: false }
 
   test('should find all properties', () => {
     expect(hasAll(obj, ['a', 'c'])).to.equal(true)
@@ -23,5 +24,13 @@ describe('function: has all', () => {
 
   test('should not find all properties in superset', () => {
     expect(hasAll(obj, ['a', 'b', 'c', 'd', 'e'])).to.equal(false)
+  })
+
+  test('should treat falsy values as defined', () => {
+    expect(hasAll(falsyObj, ['a', 'b', 'c'])).to.equal(true)
+  })
+
+  test('should detect missing property with falsy values', () => {
+    expect(hasAll(falsyObj, ['a', 'b', 'c', 'd'])).to.equal(false)
   })
 })
